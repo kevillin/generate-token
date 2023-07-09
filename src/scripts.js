82529b906d564f98f09539f0f01420bf49645f8b
Kevillin Santos <kevillin_santos@hotmail.com>
2023-07-09 14:25:08 -0300

    feat: Use DOM event listeners for token generation and the use of Clipboard-copy.

---
+++ src/scripts.js
@@ -0,0 +1,22 @@
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+
+const generateToken = (number) => {
+  const token = nanoid(number);
+  return String(token);
+};
+
+const inputNumber = document.getElementById('length');
+const generateButton = document.getElementById('generate');
+const resultArea = document.getElementById('result');
+const copyButton = document.getElementById('copy');
+const copySuccess = document.getElementById('copy-success');
+
+generateButton.addEventListener('click', () => {
+  resultArea.innerHTML = generateToken(inputNumber.value);
+});
+
+copyButton.addEventListener('click', () => {
+  copy(resultArea.innerHTML);
+  copySuccess.innerHTML = 'Copied!';
+});
